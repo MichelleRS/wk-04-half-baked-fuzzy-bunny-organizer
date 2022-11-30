539924bb08bee8d3ed82d31c55bc40f64f599f8a
Michelle Ross <miross629@gmail.com>
2022-11-30 12:33:55 -0800

    Add getFamilies function to fetch-utils

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,7 @@
 // Create your own supabase database using the provided seeds.sql file
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://nwxkvnsiwauieanvbiri.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAwMzQzNCwiZXhwIjoxOTUyNTc5NDM0fQ.8XIsU0FANdaNeQnT-DojpTL-GTlTPZ4CYZDEetpFpWc';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -10,7 +11,14 @@ export function getUser() {
 
 export async function getFamilies() {
     // fetch all families and their bunnies
-    // return checkError(response);
+    // include a .match - "this lets us use the same database for everybody in the cohort  without everybody stepping on each others' toes (since everybody will be adding bunnies to these families)"
+
+    const response = await client
+        .from('loving_families')
+        .select('*, fuzzy_bunnies (*)')
+        .match({ 'fuzzy_bunnies.user_id': client.auth.session().user.id });
+
+    return checkError(response);
 }
 
 export async function deleteBunny(id) {
